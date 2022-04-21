206f7dc9e54779b2a7739126079577e276dd04f7
Hadi <hsh18922@gmail.com>
2022-04-21 23:05:32 +0500

    Fixed bugs in Database

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const mongoose= require('mongoose');
 
 const connectDatabase= () =>{
 
-mongoose.connect(process.env.DB_URI,{
+mongoose.connect("mongodb://wolf199747:Games123@fyp-shard-00-00.jbsts.mongodb.net:27017,fyp-shard-00-01.jbsts.mongodb.net:27017,fyp-shard-00-02.jbsts.mongodb.net:27017/FYP?ssl=true&replicaSet=atlas-3w87u5-shard-0&authSource=admin&retryWrites=false&w=majority",{
 
     useNewUrlParser:true,
     useUnifiedTopology:true,
